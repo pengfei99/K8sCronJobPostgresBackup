ffda4fd151589a4ca24f61988d8fecacaf78e179
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-01-10 03:08:13 -0800

    add restore latest backup feature

--- src/DbRestoreBot.py
+++ src/DbRestoreBot.py
@@ -1,9 +1,12 @@
 import logging
-from src.db.PostgresDbManager import PostgresDbManager
-import os
-from src.storage.S3StorageEngine import S3StorageEngine
+import os.path
+import datetime
+import re
+
 from src.db.DbManagerInterface import DbManagerInterface
+from src.db.PostgresDbManager import PostgresDbManager
 from src.storage.StorageEngineInterface import StorageEngineInterface
+from src.storage.LocalStorageEngine import LocalStorageEngine
 
 log = logging.getLogger(__name__)
 
@@ -12,60 +15,71 @@ class DbRestoreBot:
 
     def __init__(self, storage_engine: StorageEngineInterface, db_manager: DbManagerInterface):
         self.db_manager = db_manager
-        self.storage_engine=storage_engine
-
+        self.storage_engine = storage_engine
 
     def list_available_backups(self, storage_path: str):
-        backup_list = []
-        # if the manager has a remote storage, use remote storage, if not use local storage
-        if self.storage_engine=="s3":
+        return self.storage_engine.list_dir(storage_path)
 
-            try:
-                backup_list = self.remote_storage.list_dir(storage_path)
-            except Exception as e:
-                log.exception(e)
-                exit(1)
-        else:
-            # logger.info('Listing S3 bucket s3://{}/{} content :'.format(aws_bucket_name, aws_bucket_path))
-            try:
-                backup_list = os.listdir(storage_path)
-            except FileNotFoundError:
-                log.error(f'Could not found {storage_path} when searching for backups.'
-                          f'Check your .config file settings')
-                exit(1)
-        return backup_list
+    @staticmethod
+    def is_backup_of_db(file_name, db_name):
+        return db_name in file_name
 
-    def restore_latest_db_backup(self,):
+    def get_latest_backup_name(self, storage_path: str, db_name: str) -> str:
+        """
+        This function search the latest backup
+
+        :param db_name:
+        :param storage_path:
+        :return:
+        """
+        all_backups = self.list_available_backups(storage_path)
+        backup_list = [f for f in all_backups if self.is_backup_of_db(f, db_name)]
+        current_date = datetime.datetime.now()
+        latest_backup_path = ""
+        min_time = datetime.timedelta(days=30)
+        for file_name in backup_list:
+            time_stamp = datetime.datetime.strptime(file_name.split("_")[0], '%Y-%M-%d')
+            # arithmetic operations on datetime will return a timedelta type
+            if min_time > current_date - time_stamp:
+                min_time = current_date - time_stamp
+                latest_backup_path = os.path.join(storage_path, file_name)
+        return latest_backup_path
+
+    def restore_latest_db_backup(self, db_name: str, backup_file_path: str):
+        # in our case, we name the postgres custom format with extension .pgdump; the plain text sql format with .sql
+        if backup_file_path.endswith(".pgdump"):
+            return self.db_manager.restore_db(db_name, backup_file_path, backup_format="psql")
+        elif backup_file_path.endswith(".sql"):
+            return self.db_manager.restore_db(db_name, backup_file_path, backup_format="sql")
 
 
 def main():
+    # create an instance of local storage
+    local = LocalStorageEngine()
     user_name = "user-pengfei"
     user_password = "changeMe"
     host_name = "10.233.30.220"
+
+    # create an instance of postgresDbManager
+    p_manager = PostgresDbManager(user_name, user_password, host_name=host_name, port="5432")
+
+    # create an instance of DbRestoreBot
+    restore_bot = DbRestoreBot(local, p_manager)
+
+    # get latest backup
+    storage_path = "/tmp/sql_bkp"
     db_name = "north_wind"
-    output_path = "/tmp"
-    p_manager = PostgresDbManager(user_name, user_password, remote_storage=None, host_name=host_name)
-    p_manager.backup_db(db_name, output_path, False)
+    latest_backup = restore_bot.get_latest_backup_name(storage_path, db_name)
+    print(latest_backup)
 
-    endpoint = "https://" + os.getenv("AWS_S3_ENDPOINT")
-    access_key = os.getenv("AWS_ACCESS_KEY_ID")
-    secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-    session_token = os.getenv("AWS_SESSION_TOKEN")
-    s3 = S3StorageEngine(endpoint, access_key, secret_key, session_token)
-    source_path = "/tmp/2022-01-06_north_wind_pg_bck.sql"
-    destination_path = "s3a://pengfei/me"
-    # download the backup file and restore
-    s3.download_data("s3a://pengfei/me/2022-01-06_north_wind_pg_bck.sql", "/home/coder/work/tmp.sql")
-    # create db
-    # PostgresDbManager.create_db(user_name, user_password, 'test', host_name)
-    # rename db
-    # PostgresDbManager.rename_db(user_name, user_password, "test", "toto",host_name)
-
-    PostgresDbManager.restore_db_with_sql_format(user_name, user_password, "test",
-                                                 "/home/coder/work/dumps/db_backup.sql", host_name)
-
-    # pg_dump --dbname=postgresql://user-pengfei:gv8eba5xmsw4kt2uk1mn@10.233.30.220:5432/north_wind -f /tmp/dump.sql -Fc -v
-    # pg_restore --no-owner --dbname=postgresql://user-pengfei:gv8eba5xmsw4kt2uk1mn@10.233.30.220:5432/north_wind /tmp/dump.sql
+    # start restore process
+    # step1 download data
+    local_path = f"/tmp/latest_{db_name}_backup.sql"
+    if local.download_data(latest_backup, local_path):
+        # step2: restore
+        restore_bot.restore_latest_db_backup()
+    else:
+        log.error("Fail to download the backup file")
 
 
 if __name__ == "__main__":
