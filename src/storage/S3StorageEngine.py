285a76383ef3be32bb45ee11826312d20daef7c6
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-01-11 03:43:42 -0800

    add s3 path pattern check

--- src/storage/S3StorageEngine.py
+++ src/storage/S3StorageEngine.py
@@ -1,4 +1,5 @@
 import os
+import re
 from typing import Optional
 
 import boto3
@@ -30,12 +31,27 @@ class S3StorageEngine(StorageEngineInterface):
                                           region_name=region_name)
 
     def upload_data(self, source_path: str, destination_path: str) -> bool:
-        bucket_name, bucket_key = self.parse_path(destination_path)
-        return self.upload_file_to_s3(bucket_name, bucket_key, source_path, delete_origin=True)
+        """
+
+        :param source_path: The local data path include parent dir and file name
+        :param destination_path: The s3 remote path that includes bucket_name and bucket_key(parent directory)
+                not the file name
+        :return: return true if upload complete, return false if failed
+        """
+        try:
+            bucket_name, bucket_key = self.parse_path(destination_path)
+            return self.upload_file_to_s3(bucket_name, bucket_key, source_path, delete_origin=True)
+        except ValueError as e:
+            log.error(e)
+            return False
 
     def download_data(self, source_path: str, destination_path: str) -> bool:
-        bucket_name, bucket_key = self.parse_path(source_path)
-        return self.download_file_from_s3(bucket_name, bucket_key, destination_path)
+        try:
+            bucket_name, bucket_key = self.parse_path(source_path)
+            return self.download_file_from_s3(bucket_name, bucket_key, destination_path)
+        except ValueError as e:
+            log.error(e)
+            return False
 
     def list_dir(self, source_path: str) -> Optional[list]:
 
@@ -55,11 +71,16 @@ class S3StorageEngine(StorageEngineInterface):
     def parse_path(path: str):
         # note the path must have the format protocol://{bucket_name}/{bucket_key}
         # for example s3a://user-pengfei/tmp/sparkcv/input is a valid path for S3StorageEngine
-        short_path = path.split("//")[-1]
-        index = short_path.index("/")
-        bucket_name = short_path[0:index]
-        bucket_key = short_path[index + 1:]
-        return bucket_name, bucket_key
+        s3_path_pattern = "^s3[a-z]?://([^/]+)/(.*?([^/]+)/?)$"
+        if re.match(s3_path_pattern, path):
+            short_path = path.split("//")[-1]
+            index = short_path.index("/")
+            bucket_name = short_path[0:index]
+            bucket_key = short_path[index + 1:]
+            return bucket_name, bucket_key
+        else:
+            log.error("The given s3 path is not a validate path")
+            raise ValueError
 
     @staticmethod
     def build_s3_object_key(source_file_path: str, bucket_key: str):
@@ -87,7 +108,8 @@ class S3StorageEngine(StorageEngineInterface):
         Upload a file to an AWS S3 bucket.
 
         :param bucket_name: The name of the bucket that you want to write
-        :param bucket_key: The destination path of the data relative to the bucket name
+        :param bucket_key: The destination path of the data relative to the bucket name. It's the parent dir of
+                           the uploaded file
         :param source_file_path: indicates the source file path, all files under the path will be uploaded to s3
         :param delete_origin: default value is False. If set to True, after upload, the source file will be deleted.
         :return: None
@@ -126,3 +148,9 @@ class S3StorageEngine(StorageEngineInterface):
 #
 # The put_object method maps directly to the low-level S3 API request. It does not handle multipart uploads for you.
 # It will attempt to send the entire body in one request.
+
+# The difference between s3:// and (s3n/s3a):// is that s3 is a block-based overlay on top of Amazon S3,
+# while s3n/s3a are not (they are object-based).
+#
+# The difference between s3n and s3a is that s3n supports objects up to 5GB in size, while s3a supports objects up
+# to 5TB and has higher performance (both are because it uses multi-part upload). s3a is the successor to s3n.
