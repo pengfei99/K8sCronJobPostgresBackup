560190072d4f997e74319328dbf35fe403571575
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-01-12 11:07:39 -0800

    add new feature to allow bot to create backup with custom name

--- src/DbBackupBot.py
+++ src/DbBackupBot.py
@@ -1,4 +1,4 @@
-import locale
+from datetime import datetime
 import logging
 from typing import Optional
 
@@ -23,24 +23,29 @@ class DbBackupBot:
         self.storage_engine = storage_engine
         self.db_manager = db_manager
 
-    def make_backup(self, db_name: str, backup_file_parent_dir: str, tmp_dir='/tmp') -> Optional[str]:
+    def make_auto_backup(self, db_name: str, backup_file_parent_dir: str, tmp_dir='/tmp') -> Optional[str]:
+        current_date = str(datetime.date(datetime.now()))
+        backup_file_name = f"{current_date}_{db_name}_pg_bck.sql"
+        return self.make_backup_with_custom_file_name(db_name, backup_file_parent_dir, backup_file_name, tmp_dir)
+
+    def make_backup_with_custom_file_name(self, db_name: str, backup_file_parent_dir: str, backup_file_name: str,
+                                          tmp_dir='/tmp') -> Optional[str]:
         # step 1: check if the target db exist
         if self.db_manager.has_db(db_name):
-            # step 2: check the storage engine type, if remote(s3), need to add an upload step
-            storage_engine_type = self.storage_engine.get_storage_engine_type()
-            # s3 case
-            if storage_engine_type == 's3':
-                tmp_file_path = self.db_manager.backup_db(db_name, tmp_dir)
-                if self.storage_engine.upload_data(tmp_file_path, backup_file_parent_dir):
-                    backup_file_path = f"{backup_file_parent_dir}/{os.path.basename(tmp_file_path)}"
+            # if db exist, start step 2: build the backup file
+            tmp_bk_file_path = self.db_manager.backup_db(db_name, tmp_dir, backup_file_name)
+            if tmp_bk_file_path:
+                # if backup success, start step 3: upload backup file
+                destination_path = f"{backup_file_parent_dir}/{backup_file_name}"
+                if self.storage_engine.upload_data(tmp_bk_file_path, destination_path):
+                    log.info(f"Backup complete. Creat backup file {destination_path} for db {db_name}")
+                    return destination_path
                 else:
-                    log.error(f"Unable to upload backup file {tmp_file_path} to s3 {backup_file_parent_dir} ")
-                    backup_file_path = None
-            # local case
-            elif storage_engine_type == 'local':
-                backup_file_path = self.db_manager.backup_db(db_name, backup_file_parent_dir)
-            log.info(f"Backup complete. Creat backup file {backup_file_path} for db {db_name}")
-            return backup_file_path
+                    log.error(f"Unable to upload backup file {tmp_bk_file_path} to {backup_file_parent_dir}")
+                    return None
+            else:
+                log.error(f"Unable to create backup for database {db_name}")
+                return None
         else:
             log.exception(f"The input database {db_name} does not exist in the target server")
             return None
@@ -52,7 +57,7 @@ def main():
     user_name = "pliu"
     user_password = "pliu"
     host_name = "127.0.0.1"
-    port="5432"
+    port = "5432"
 
     # create an instance of postgresDbManager
     p_manager = PostgresDbManager(user_name, user_password, host_name=host_name, port=port)
@@ -60,7 +65,7 @@ def main():
     # create an instance of DbBackupBot
     backup_bot = DbBackupBot(local, p_manager)
 
-    backup_bot.make_backup("test","/tmp/sql_backup")
+    backup_bot.make_backup("test", "/tmp/sql_backup")
 
     # temp local path if you use remote storage
     # get s3 creds
@@ -70,8 +75,6 @@ def main():
     # session_token = os.getenv("AWS_SESSION_TOKEN")
     # build s3 client
     # s3 = S3StorageEngine(endpoint, access_key, secret_key, session_token)
-
-
 
 
 if __name__ == "__main__":
