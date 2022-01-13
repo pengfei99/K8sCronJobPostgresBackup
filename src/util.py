0002e5a1ccdce23c9dac1254c22f9a8e1d2f89c9
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-01-13 06:35:45 -0800

    extract backup file date limit setting to util

--- src/util.py
+++ src/util.py
@@ -1,4 +1,3 @@
-
 def get_date_format():
     """
     This function is called by DbBackupBot and DbRestoreBot for creating and restoring backup file with timestamp
@@ -6,3 +5,14 @@ def get_date_format():
     :return: a string which is the format for timestamp in the backup file
     """
     return '%Y_%m_%d_%H_%M_%S'
+
+
+def get_date_limit_of_backup_file():
+    """
+    This function returns the date limit (in days) for the backup file. By default, the value is 30. It means the
+    DbRetoreBot will only search the backup that are less than 30 days from the current date.
+
+    :return: the date limit of the backup file
+    """
+    day = 30
+    return day
